05a470093af35d17be90f7a7c9fec34ee829d559
Jingyi-MASY <ji.jing@northeastern.edu>
2021-09-13 03:26:08 -0400

    toast added

--- app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/MainActivity.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/MainActivity.java
@@ -2,7 +2,10 @@ package edu.neu.madcourse.numad21fa_jingyiji;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+    public void aboutToast(View view) {
+        Context about = getApplicationContext();
+        CharSequence text = "Jingyi Ji: ji.jing@northeastern.edu";
+        int duration = Toast.LENGTH_SHORT;
+
+        Toast toast = Toast.makeText(about, text, duration);
+        toast.show();
     }
 }
