1a0c408bb7e862b0f0b9c5ec6165012d13400840
Jingyi-MASY <ji.jing@northeastern.edu>
2021-10-03 20:41:47 -0400

    20:41

--- app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/Input.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/Input.java
@@ -5,35 +5,41 @@ import androidx.coordinatorlayout.widget.CoordinatorLayout;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
+import android.webkit.URLUtil;
 import android.widget.EditText;
 
 import com.google.android.material.snackbar.Snackbar;
 
 public class Input extends AppCompatActivity {
-    private int pos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_input);
-
-        int pos = getIntent().getIntExtra("pos", 0);
-
     }
+    //TODO: Snap bar; All URL format
 
     public void confirm(View view) {
         EditText link = (EditText) findViewById(R.id.input_link);
 
         EditText name = (EditText) findViewById(R.id.input_name);
+        Log.d("jj", String.valueOf(URLUtil.isValidUrl(link.getText().toString())));
 
-        //TODO: Input check, snackbar
-
-
-        LinkCollector.itemList.add(pos, new ItemCard(link.getText().toString(), name.getText().toString()));
-        LinkCollector.viewAdapter.notifyItemInserted(pos);
-        this.finish();
+        if(Patterns.WEB_URL.matcher(link.getText().toString()).matches()) {
+            Intent intent = new Intent();
+            intent.putExtra("name", name.getText().toString());
+            intent.putExtra("link", link.getText().toString());
+            setResult(RESULT_OK, intent);
+            this.finish();
+        }
+        Snackbar.make(view, "Invalid link: Please Re-enter.",
+                Snackbar.LENGTH_LONG)
+                .show();
     }
+
 
 
 }
--- app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/LinkCollector.java
+++ app/src/main/java/edu/neu/madcourse/numad21fa_jingyiji/LinkCollector.java
@@ -1,5 +1,9 @@
 package edu.neu.madcourse.numad21fa_jingyiji;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.DefaultItemAnimator;
@@ -7,23 +11,30 @@ import androidx.recyclerview.widget.ItemTouchHelper;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 import java.util.ArrayList;
 
 public class LinkCollector extends AppCompatActivity {
-    protected static ArrayList<ItemCard> itemList = new ArrayList<>();
+    private ArrayList<ItemCard> itemList = new ArrayList<>();
 
     private RecyclerView recyclerView;
-    protected static ViewAdapter viewAdapter;
+    private ViewAdapter viewAdapter;
     private RecyclerView.LayoutManager layoutManger;
     private FloatingActionButton addButton;
+
+    private static final String KEY_OF_INSTANCE = "KEY_OF_INSTANCE";
+    private static final String NUMBER_OF_ITEMS = "NUMBER_OF_ITEMS";
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,6 +42,8 @@ public class LinkCollector extends AppCompatActivity {
         setContentView(R.layout.activity_link_collector);
 
         itemList = new ArrayList<>();
+
+        initialItemData(savedInstanceState);
 
         addButton = findViewById(R.id.addButton);
         addButton.setOnClickListener(new View.OnClickListener() {
@@ -45,8 +58,6 @@ public class LinkCollector extends AppCompatActivity {
 
     }
 
-    //TODO: When the user taps a link in the list, launch the URL in a web browser (use an intent, not android:autolink).
-
     private void createRecyclerView() {
 
 
@@ -59,10 +70,10 @@ public class LinkCollector extends AppCompatActivity {
         ItemClickListener itemClickListener = new ItemClickListener() {
             @Override
             public void onItemClick(int position) {
-                //attributions bond to the item has been changed
                 itemList.get(position).onItemClick(position);
-
                 viewAdapter.notifyItemChanged(position);
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse(itemList.get(position).getItemLink()));
+                startActivity(browserIntent);
             }
         };
         viewAdapter.setOnItemClickListener(itemClickListener);
@@ -73,39 +84,74 @@ public class LinkCollector extends AppCompatActivity {
 
     }
 
-    //TODO: rotation
+    //rotation
 
+    private void initialItemData(Bundle savedInstanceState) {
+
+        // Not the first time to open this Activity
+        if (savedInstanceState != null && savedInstanceState.containsKey(NUMBER_OF_ITEMS)) {
+            if (itemList == null || itemList.size() == 0) {
+
+                int size = savedInstanceState.getInt(NUMBER_OF_ITEMS);
+
+                // Retrieve keys we stored in the instance
+                for (int i = 0; i < size; i++) {
+                    String name = savedInstanceState.getString(KEY_OF_INSTANCE + i + "0");
+                    String link = savedInstanceState.getString(KEY_OF_INSTANCE + i + "1");
+
+                    ItemCard itemCard = new ItemCard(link, name);
+
+                    itemList.add(itemCard);
+                }
+            }
+        }
+
+    }
     // Handling Orientation Changes on Android
-//    @Override
-//    protected void onSaveInstanceState(@NonNull Bundle outState) {
-//
-//
-//        int size = itemList == null ? 0 : itemList.size();
-//        outState.putInt(NUMBER_OF_ITEMS, size);
-//
-//        // Need to generate unique key for each item
-//        // This is only a possible way to do, please find your own way to generate the key
-//        for (int i = 0; i < size; i++) {
-//            // put image information id into instance
-//            outState.putInt(KEY_OF_INSTANCE + i + "0", itemList.get(i).getImageSource());
-//            // put itemName information into instance
-//            outState.putString(KEY_OF_INSTANCE + i + "1", itemList.get(i).getItemName());
-//            // put itemDesc information into instance
-//            outState.putString(KEY_OF_INSTANCE + i + "2", itemList.get(i).getItemDesc());
-//            // put isChecked information into instance
-//            outState.putBoolean(KEY_OF_INSTANCE + i + "3", itemList.get(i).getStatus());
-//        }
-//        super.onSaveInstanceState(outState);
-//
-//    }
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
 
 
+        int size = itemList == null ? 0 : itemList.size();
+        outState.putInt(NUMBER_OF_ITEMS, size);
 
+        // Need to generate unique key for each item
+        // This is only a possible way to do, please find your own way to generate the key
+        for (int i = 0; i < size; i++) {
+
+            outState.putString(KEY_OF_INSTANCE + i + "0", itemList.get(i).getItemName());
+            outState.putString(KEY_OF_INSTANCE + i + "1", itemList.get(i).getItemLink());
+
+        }
+        super.onSaveInstanceState(outState);
+
+    }
+
+    private int pos;
+
+    ActivityResultLauncher<Intent> inputResultLauncher = registerForActivityResult(
+            new ActivityResultContracts.StartActivityForResult(),
+            new ActivityResultCallback<ActivityResult>() {
+                @Override
+                public void onActivityResult(ActivityResult result) {
+                    if (result.getResultCode() == Activity.RESULT_OK) {
+
+                        Intent data = result.getData();
+                        itemList.add(pos, new ItemCard(data.getStringExtra("link"), data.getStringExtra("name")));
+                        viewAdapter.notifyItemInserted(pos);
+
+                        Snackbar.make(findViewById(R.id.myCoordinatorLayout), "Successful Added.",
+                                Snackbar.LENGTH_LONG)
+                                .show();
+
+                    }
+                }
+            });
 
     private void addItem(int pos) {
+        this.pos = pos;
         Intent intent = new Intent(this, Input.class);
-        startActivity(intent);
-        intent.putExtra("pos", pos);
+        inputResultLauncher.launch(intent);
     }
 
 
